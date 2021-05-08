5936e7f94b54ec1522760a6a759325bfb2b990a7
Ming-der Wang <mingderwang@gmail.com>
2021-05-08 15:18:43 +0800

    add ipdid qrcode command

---
+++ src/commands/qrcode.js
@@ -0,0 +1,45 @@
+const { Command, flags } = require("@oclif/command");
+
+class QRCommand extends Command {
+  async init() {
+    async function logChunks(readable) {
+      for await (const chunk of readable) {
+        console.log(chunk)
+        console.log(chunk.toString('utf-8'))
+        QRCommand.stdin = chunk
+      }
+    }
+
+    const { flags } = this.parse(QRCommand);
+    if (flags.context === undefined) {
+      await logChunks(process.stdin)
+    }
+  }
+
+  async run() {
+    const qrcode = require('qrcode-terminal');
+    const { flags } = this.parse(QRCommand);
+    if (!flags.context) {
+      if (QRCommand.stdin) {
+        flags.context = QRCommand.stdin.toString('utf-8')
+      } else {
+        console.error(`-c or --context is required, or pipe a text in`);
+      }
+    }
+    const context = flags.context || '0x8587eA108898749538372Cd3Df459870C4a1A56F';
+    this.log(`genreate QR-code in terminal for string: ${context}`);
+    const cleanContext = context.replace(/[\n\r]/gi, "");
+    qrcode.generate(cleanContext); 
+  }
+}
+
+QRCommand.description = `to generate your DID QR-code
+...
+--context string (default for testing: '0x8587eA108898749538372Cd3Df459870C4a1A56F')
+`;
+
+QRCommand.flags = {
+  context: flags.string({ char: "c", description: "the context for generating a QR-code" }),
+};
+
+module.exports = QRCommand;
